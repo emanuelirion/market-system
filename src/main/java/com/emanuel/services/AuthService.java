cf43dc0c28a02f1a108ec36ba82bc3d67288c1a0
Emanuel <2310100375>
2026-01-17 15:09:05 -0300

    Metodo de login.

--- src/main/java/com/emanuel/services/AuthService.java
+++ src/main/java/com/emanuel/services/AuthService.java
@@ -8,7 +8,7 @@ import com.emanuel.payload.response.AuthResponse;
 public interface AuthService {
 
     AuthResponse signup(UserDto userDto) throws UserException;
-    AuthResponse login(UserDto userDto);
+    AuthResponse login(UserDto userDto) throws UserException;
 
 
 }
--- src/main/java/com/emanuel/services/impi/AuthServiceImpl.java
+++ src/main/java/com/emanuel/services/impi/AuthServiceImpl.java
@@ -12,11 +12,15 @@ import com.emanuel.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
@@ -32,18 +36,18 @@ public class AuthServiceImpl implements AuthService {
         if (user != null) {
             throw new UserException("email id already registered !");
         }
-       if(userDto.getRole().equals(UserRole.ROLE_ADMIN)) {
-           throw new UserException("role admin is not allowed !");
-       }
-       User newUser = new User();
-       newUser.setEmail(userDto.getEmail());
-       newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
-       newUser.setRole(userDto.getRole());
-       newUser.setFullName(userDto.getFullName());
-       newUser.setPhone(userDto.getPhone());
-       newUser.setLastLogin(LocalDateTime.now());
-       newUser.setCreatedAt(LocalDateTime.now());
-       newUser.setUpdatedAt(LocalDateTime.now());
+        if (userDto.getRole().equals(UserRole.ROLE_ADMIN)) {
+            throw new UserException("role admin is not allowed !");
+        }
+        User newUser = new User();
+        newUser.setEmail(userDto.getEmail());
+        newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        newUser.setRole(userDto.getRole());
+        newUser.setFullName(userDto.getFullName());
+        newUser.setPhone(userDto.getPhone());
+        newUser.setLastLogin(LocalDateTime.now());
+        newUser.setCreatedAt(LocalDateTime.now());
+        newUser.setUpdatedAt(LocalDateTime.now());
         User savedUser = userRepository.save(newUser);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDto.getEmail(), userDto.getPassword());
@@ -61,7 +65,39 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public AuthResponse login(UserDto userDto) {
-        return null;
+    public AuthResponse login(UserDto userDto) throws UserException {
+        String email = userDto.getEmail();
+        String password = userDto.getPassword();
+        Authentication authentication = authenticate(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String role = authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToken(authentication);
+        User user = userRepository.findByEmail(email);
+        user.setLastLogin(LocalDateTime.now());
+        userRepository.save(user);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage(("login Successfully !"));
+        authResponse.setUser(UserMapper.toDTO(user));
+
+        return authResponse;
+    }
+
+    private Authentication authenticate(String email, String password) throws UserException {
+
+        UserDetails userDetails = costumUserImplementation.loadUserByUsername(email);
+
+        if (userDetails == null) {
+            throw new UserException("email id doesn'd exist" + email);
+        }
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new UserException("password doesn't match");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
+
